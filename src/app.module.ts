5f2b7b453129bd4107c3006f27d4434496e4f036
Nguyen Tien Dat <nguyentiendat@MacBook-Pro-cua-Nguyen.local>
2025-06-07 15:51:47 +0700

    Delete Room module and Theater module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,9 @@ import { MovieModule } from '~/modules/movie/movie.module'
 import { GenreModule } from '~/modules/genre/genre.module'
 import { ReservationModule } from '~/modules/reservation/reservation.module'
 import { ReservationSeatModule } from '~/modules/reservation_seat/reservation_seat.module'
-import { RoomModule } from '~/modules/room/room.module'
 import { SeatModule } from '~/modules/seat/seat.module'
 import { ShowtimeModule } from '~/modules/showtime/showtime.module'
 import { TemporaryLockModule } from '~/modules/temporary_lock/temporary_lock.module'
-import { TheaterModule } from '~/modules/theater/theater.module'
 import { AuthModule } from '~/modules/auth/auth.module'
 import { ConfigModule } from '@nestjs/config'
 import { MailModule } from '~/modules/mail/mail.module'
@@ -23,11 +21,9 @@ import { MailModule } from '~/modules/mail/mail.module'
     GenreModule,
     ReservationModule,
     ReservationSeatModule,
-    RoomModule,
     SeatModule,
     ShowtimeModule,
     TemporaryLockModule,
-    TheaterModule,
     MailModule,
     ConfigModule.forRoot({ isGlobal: true })
   ],
